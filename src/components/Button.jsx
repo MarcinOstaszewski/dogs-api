a4831b604b3890bb4fba7ec5313feb015c71701d
Marcin Ostaszewski <m.ostaszewski@azagroup.eu>
2021-11-17 08:07:01 +0100

    add Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = props => {
+    return (
+        <div className="App-button" >
+            {props.race}
+        </div>
+    )
+}
+
+export { Button };
