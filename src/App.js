64af6e289cb06a4641523db9df91310b442b25ef
Marcin Ostaszewski <m.ostaszewski@azagroup.eu>
2021-11-17 08:06:47 +0100

    add fetch data and create buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect} from 'react';
+import { Button } from './components/Button';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [buttons, setButtons] = useState({dogsRaces: []});
+
+    const createButtons = data => {
+        let dogsButtons = Object.keys(data).map( breed => {
+            if (data[breed].length > 0) {
+                return data[breed].map( subBreed => {
+                    return <Button breed={`${subBreed} ${breed}`}/>
+                })
+            } else {
+                return <Button breed={breed}/>
+            }
+        })
+        setButtons(dogsButtons)
+    }
+
+    useEffect(() => {
+        (async () => {
+            const response = await axios('https://dog.ceo/api/breeds/list/all');
+            createButtons(response.data.message);
+        })();
+    }, []);
+
+    return (
+        <div className = "App" >
+            <header className = "App-header">
+                Click on a button below to see a random photo of the dog's breed.
+            </header>
+            <main className = "App-main">
+                {buttons}
+            </main>
+        </div>
+    );
 }
 
 export default App;
